151866a284e610bf1c75085e4c0c9264fdac9b8f
vv_msk94 <radyuk.v.v@yandex.ru>
2024-07-13 14:34:58 +0300

    Hello World

---
+++ src/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+Hello::Hello()
+{
+    std::cout<<"Hello World"<<std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+
+int main()
+{
+	std::cout<<"Hello World!"<<std::endl;
+	return 0;
+}
