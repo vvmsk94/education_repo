ae23bf488c294f76e78122300c78c2f0f4479f1b
vvmsk94 <102922125+vvmsk94@users.noreply.github.com>
2024-07-13 17:06:17 +0300

    Update release.yaml

---
+++ my_tests/test_hello.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "IpFilter.h"
+
+TEST(Hello, foo)
+{
+    ASSERT_STREQ("Hello", "Hello");
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
