18e08d7f5c124f56ad9c79a55de92feaa7286dd4
vvmsk94 <102922125+vvmsk94@users.noreply.github.com>
2024-07-13 17:06:17 +0300

    Update release.yaml

---
+++ static-analysis.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+export CLANGD_FLAGS="-I/usr/include/c++/11"
+
+CLANG_TIDY="clang-tidy"
+SCRIPT_DIR="$(dirname $0)"
+
+find "$SCRIPT_DIR" -not -path "*build/*" -and \
+                   -not -path "*test_package/*" -and \
+                   -not -path "*tests/*" -and \
+                   \( -name "*.h" -or -name "*.cpp" \) -print0 | \
+xargs -0 $CLANG_TIDY $@
+
