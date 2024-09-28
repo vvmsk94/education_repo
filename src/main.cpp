c2e56ca12803405042533222e6f39f23c831510f
vv_msk94 <radyuk.v.v@yandex.ru>
2024-09-28 15:12:23 +0300

    homework 8

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,119 @@
-﻿#include <iostream>
+#include "mask_filesystem_traverser.h"
+#include "duplicate_finder.h"
+#include "hash/crc32.h"
+#include "hash/crc16.h"
 
-#include <array>
-#include <string>
-#include <cstring>
-#include <vector>
-#include <list>
-#include <type_traits>
+#include <boost/program_options.hpp>
 
+#include <iostream>
+#include <filesystem>
+#include <regex>
 
-template <typename T>
-typename std::enable_if<std::is_integral<T>::value, void>::type
-printIP(const T& val, std::ostream& stream){
-	constexpr size_t len(sizeof(T));
-	static_assert(len>0," sizeof returned negative value?! ");
-	
-	std::array<unsigned char, len> bytes;	
-	std::memcpy(bytes.data(), &val, len);
-	
-	for(auto i=len-1;i>0;--i)
-		stream << std::to_string(bytes.at(i)) << ".";
-	stream << std::to_string(bytes.at(0));
-	stream << std::endl;
-}
+namespace fs = std::filesystem;
 
-
-void printIP (const std::string& val, std::ostream& stream){
-	stream << val << std::endl;
-}
-
-template<class T>
-typename std::enable_if<std::is_same<T, std::vector <typename T::value_type> >::value
-						|| std::is_same<T, std::list <typename T::value_type> >::value
-					,void>::type 
-printIP(const T& container, std::ostream& stream){
-	
-	auto first = container.begin();
-	auto last = container.end();
-	
-	if(first!=last){
-		for(--last; first!=last; ++first)
-			stream << std::to_string(*first) <<".";
-		stream << std::to_string(*first) <<std::endl;
-	}
-}
-
-
-int main(int argc, char const *argv[])
+template <typename Hash>
+void job(std::size_t block_size, my::IFilesystemTraverser* filesystem_traverser)
 {
-    try
-    {	
-		printIP(char(-1), std::cout);
-		printIP(short(0), std::cout);
-		printIP(int(2130706433), std::cout);
-		printIP((long long)(8875824491850138409), std::cout);
+    my::DuplicateFinder<Hash> duplicate_finder(block_size);
+    duplicate_finder.add_filesystem_traverser(filesystem_traverser);
 
-		printIP(std::string("1.2.3.4.5.6.7.8.99"), std::cout);
-				
-		printIP(std::vector<int>{0x012345,0x987654,0x112233}, std::cout);
-		printIP(std::vector<unsigned char>{0x11,0x22,0x33,0x44}, std::cout);
-		printIP(std::list<int>{0x012345,0x987654,0x112233}, std::cout);
-		printIP(std::list<long long>{75824491850138409,75824491850138408,75824491850138410,75824491850138407}, std::cout);
-				
-    }
-    catch(const std::exception &e)
+    std::vector<std::vector<fs::path>> equal = duplicate_finder.get_duplicates();
+    for (const std::vector<fs::path>& group : equal)
     {
-        std::cerr << e.what() << std::endl;
+        for (const fs::path& path : group)
+            std::cout << path << '\n';
+        std::cout << '\n';
+    }
+}
+
+int main(int argc, char* argv[]) try
+{
+    namespace po = boost::program_options;
+    po::options_description opt_desc("Allowed options");
+    bool recursive = false;
+    bool case_sensitive = false;
+    opt_desc.add_options()
+        ("help",                                                                            "Print this message")
+        ("size",           po::value<std::size_t>()->required()->default_value(4096),       "Block size (in bytes) used to compare files (at least 1)")
+        ("hash",           po::value<std::string>()->required()->default_value("crc32"),    "Hash algorithm used to compare byte blocks, one of 'crc32', 'crc16'")
+        ("min_file_size",  po::value<std::size_t>()->required()->default_value(1),          "Minimum file size to compare")
+        ("root_dir",       po::value<std::vector<std::string>>()->required()->multitoken(), "Directories to search duplicates into")
+        ("exclude_dir",    po::value<std::vector<std::string>>()->multitoken(),             "Direcroties to exclude from search")
+        ("mask_include",   po::value<std::vector<std::string>>()->multitoken(),             "Include only files corresponding to these masks in search")
+        ("mask_exclude",   po::value<std::vector<std::string>>()->multitoken(),             "Exclude files corresponding to these masks from search")
+        ("recursive",      po::bool_switch(&recursive),                                     "Use this option to enable recursive subdirectory scanning")
+        ("case_sensitive", po::bool_switch(&case_sensitive),                                "Use this option to make file masks case sensitive")
+    ;
+
+    po::variables_map var_map;
+    try
+    {
+        auto parsed = po::command_line_parser(argc, argv)
+            .options(opt_desc)
+            .run();
+        po::store(parsed, var_map);
+        if (var_map.count("help") != 0)
+        {
+            std::cout << opt_desc << "\n";
+            return 0;
+        }
+        po::notify(var_map);
+    }
+    catch (const po::error& error)
+    {
+        std::cerr << "Error while parsing command-line arguments: "
+                  << error.what() << "\nPlease use --help to see help message\n";
+        return 1;
     }
 
-    return 0;
+    std::size_t block_size = var_map["size"].as<std::size_t>();
+    if (block_size == 0)
+    {
+        std::cerr << "Block size must be at least 1\n";
+        return 1;
+    }
+    std::size_t min_file_size = var_map["min_file_size"].as<std::size_t>();
+
+    std::vector<std::string> root_directories    = var_map["root_dir"].as<std::vector<std::string>>();
+
+    std::vector<std::string> exclude_directories = var_map.count("exclude_dir") == 0
+                                                   ? std::vector<std::string>()
+                                                   : var_map["exclude_dir"].as<std::vector<std::string>>();
+
+    std::vector<std::string> masks_include       = var_map.count("mask_include") == 0
+                                                   ? std::vector<std::string>()
+                                                   : var_map["mask_include"].as<std::vector<std::string>>();
+
+    std::vector<std::string> masks_exclude       = var_map.count("mask_exclude") == 0
+                                                   ? std::vector<std::string>()
+                                                   : var_map["mask_exclude"].as<std::vector<std::string>>();
+
+    my::MaskFilesystemTraverser filesystem_traverser(recursive);
+
+    for (std::string& root_directory : root_directories)
+        filesystem_traverser.add_root_directory(fs::path(std::move(root_directory)));
+    for (std::string& exclude_directory : exclude_directories)
+        filesystem_traverser.add_exclude_directory(fs::path(std::move(exclude_directory)));
+
+    std::regex::flag_type re_flags = (case_sensitive ? std::regex_constants::ECMAScript : std::regex_constants::icase);
+    for (const std::string& mask_include : masks_include)
+        filesystem_traverser.add_file_mask_include(std::regex(mask_include, re_flags));
+    for (const std::string& mask_exclude : masks_exclude)
+        filesystem_traverser.add_file_mask_exclude(std::regex(mask_exclude, re_flags));
+
+    filesystem_traverser.set_min_file_size(min_file_size);
+
+    std::string hash_algo = var_map["hash"].as<std::string>();
+
+    if (hash_algo == "crc32")
+        job<my::Crc32>(block_size, &filesystem_traverser);
+    else if (hash_algo == "crc16")
+        job<my::Crc16>(block_size, &filesystem_traverser);
+    else
+        throw std::invalid_argument(std::string("Incorrect hash algorithm ") + hash_algo + ". Use --help options for more details");
+}
+catch (const std::exception& e)
+{
+    std::cerr << e.what() << '\n';
+    return 1;
 }
